bbce3fe8e5d8a9a64d0c74a9df3f8f9119423c41
kavishka-sulakshana <kss20001218@gmail.com>
2023-07-27 01:03:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { wss } from "./init";
+
+// routes
+import mainRoute from "./routes/main.route";
+
+const app = express();
+app.use(express.json());
+
+const server = app.listen(4001, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(
+        "Websocket event broadcaster REST API listening on http://%s:%s",
+        host,
+        port
+    );
+});
+
+// routes
+app.use("/", mainRoute);
+
+// websocket listening mode
+wss.on("connection", function connection(ws) {
+    console.log("Websocket client connected");
+    ws.on("message", function incoming(message) {
+        console.log("received: %s", message);
+    });
+    ws.on("close", function close() {
+        console.log("Websocket client disconnected");
+    });
+    ws.send("connected");
+});
---
+++ routes/main.route.js
@@ -0,0 +1,23 @@
+import express from "express";
+import { wss } from "./init";
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+    console.log("Event: main event (/get) ");
+    wss.clients.forEach(function each(client) {
+        client.send("From Server");
+    });
+    res.sendStatus(200);
+});
+
+router.post("/", function (req, res) {
+    let message = req.body.message;
+    console.log("Event: main event (/post) :", message);
+    wss.clients.forEach(function each(client) {
+        client.send(message);
+    });
+    res.sendStatus(200);
+});
+
+export default router;
