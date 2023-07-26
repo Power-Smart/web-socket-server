0a4a51454787196a6dc5852538aa63184c4e9c79
kavishka-sulakshana <kss20001218@gmail.com>
2023-07-27 01:44:50 +0530

    make controllers and routes

---
+++ controller/main.controller.js
@@ -0,0 +1,18 @@
+import { wss } from "../init.js";
+
+export const mainController = (req, res) => {
+    console.log("Event: main event (/get) ");
+    wss.clients.forEach(function each(client) {
+        client.send("From Server (/get)");
+    });
+    res.sendStatus(200);
+};
+
+export const mainControllerPost = (req, res) => {
+    let message = req.body.message;
+    console.log("Event: main event (/post) :", message);
+    wss.clients.forEach(function each(client) {
+        client.send(message);
+    });
+    res.sendStatus(200);
+};
--- routes/main.route.js
+++ routes/main.route.js
@@ -1,23 +1,12 @@
 import express from "express";
-import { wss } from "../init.js";
+import {
+    mainController,
+    mainControllerPost,
+} from "../controller/main.controller.js";
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
-    console.log("Event: main event (/get) ");
-    wss.clients.forEach(function each(client) {
-        client.send("From Server (/get)");
-    });
-    res.sendStatus(200);
-});
-
-router.post("/", function (req, res) {
-    let message = req.body.message;
-    console.log("Event: main event (/post) :", message);
-    wss.clients.forEach(function each(client) {
-        client.send(message);
-    });
-    res.sendStatus(200);
-});
+router.get("/", mainController);
+router.post("/", mainControllerPost);
 
 export default router;
